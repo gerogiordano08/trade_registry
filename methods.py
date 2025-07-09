5f422d3999b81d40871de196a500d37fbd1a718f
gerogiordano08 <gerogiordano08@users.noreply.github.com>
2025-07-09 13:34:13 -0400

    definded get_today_date()

--- methods.py
+++ methods.py
@@ -5,3 +5,6 @@ def get_price(date:str, stock:str):
     ticker = yf.Ticker(stock.upper())
     data = ticker.history(start=date, period='1d')
     return data['Close'].iloc[0] if not data.empty else None
+
+def get_today_date():
+    return date.today().strftime('%Y-%m-%d')
