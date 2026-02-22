b7b44cca549025ab6532bd189e9f2786f8f14138
gerogiordano08 <gerogiordano08@users.noreply.github.com>
2026-02-22 17:43:30 -0300

    switched cron schedule for env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# cron template configuration
+envsubst '${CRON_PRICE_UPDATE_TIME} ${CRON_NEWS_SCRAPE_TIME} ${CRON_NEWS_CLEAN_TIME} ${CRON_NEWS_SCRAPE_PRICE_UPDATE_LOG_TRUNCATE_TIME} ${CRON_NEWS_CLEAN_LOG_TRUNCATE_TIME}' < /usr/src/app/crontab.template > /usr/src/app/crontab.txt
+cp /usr/src/app/crontab.txt /etc/cron.d/trade-cron
+chmod 0644 /etc/cron.d/trade-cron
+# Ensure the file ends with a newline (cron requirement)
+echo "" >> /etc/cron.d/trade-cron
+
+# config envs for cron
 touch /usr/src/app/.env
 printenv | sed 's/^\([^=]*\)=\(.*\)$/export \1="\2"/' > /usr/src/app/.env
 chown appuser:appuser /usr/src/app/.env
@@ -11,8 +20,8 @@ done
 
 service cron start
 echo "PostgreSQL ready. Excecuting migrations..."
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+gosu appuser python manage.py migrate --no-input
+gosu appuser python manage.py collectstatic --no-input
 
 
-exec "$@"
+exec gosu appuser "$@"
