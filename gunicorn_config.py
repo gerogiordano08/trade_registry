d29732a12d44d6e8d11e7a466667a774862c53b1
gerogiordano08 <gerogiordano08@users.noreply.github.com>
2026-02-23 19:05:26 -0300

    fixed migration problem, added favicon

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,19 +1,11 @@
-# gunicorn_config.py optimizado para Free Tier (0.1 CPU, 512 MB RAM)
+import os
 
-# 1. Concurrencia (Workers)
-# Usar solo 1 worker es lo más seguro por la limitación de RAM.
-workers = 1 
+workers = int(os.getenv('GUNICORN_WORKERS', '1'))
 
-# 2. Tipo de Worker
-# Gevent es ideal para tareas de I/O (esperar peticiones a yfinance)
-# porque no bloquea el único worker mientras espera una respuesta.
-worker_class = 'gevent' 
+worker_class = os.getenv('GUNICORN_WORKER_CLASS', 'gevent')
 
-# 3. Gevent Worker Connections
-# Define cuántas peticiones simultáneas puede manejar el único worker.
-# 1000 es un valor estándar para Gevent.
-worker_connections = 1000 
+# Defines simultaneous worker connections
+worker_connections = int(os.getenv('GUNICORN_WORKER_CONNECTIONS', '1000'))
 
-# 4. Enlace y Timeout
-bind = '0.0.0.0:8000'
-timeout = 90
+bind = os.getenv('GUNICORN_BIND', '0.0.0.0:8000')
+timeout = int(os.getenv('GUNICORN_TIMEOUT', '90'))
--- the_trade_registry/urls.py
+++ the_trade_registry/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 import os
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
-    
+    path('favicon.ico', RedirectView.as_view(url='/static/trade_registry/favicon.png', permanent=True)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
