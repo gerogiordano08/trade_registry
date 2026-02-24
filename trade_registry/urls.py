4a1d247e62e1c67861e72fbe4b5ed1ff49650e61
gerogiordano08 <gerogiordano08@users.noreply.github.com>
2026-02-24 19:01:50 -0300

    feat(frontend): added responsive features and templates to website

--- trade_registry/urls.py
+++ trade_registry/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('help/', views.help, name='help'),
     path('signup/', views.signup, name='signup'),
     path('settings/', views.settings, name='settings'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html', success_url= 'index'), name='password_change'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html', success_url='/'), name='password_change'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path(r'reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
